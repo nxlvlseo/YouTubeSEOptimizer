bffbd9426b3bce814855438eebf1ed625307e245
nxlvlseo <161171798+nxlvlseo@users.noreply.github.com>
2024-02-26 21:09:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
+#from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import pandas as pd
 import csv
