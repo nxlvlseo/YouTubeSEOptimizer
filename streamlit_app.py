69c836580c9d987f71e1eed9a0447266d8d0179f
nxlvlseo <161171798+nxlvlseo@users.noreply.github.com>
2024-02-26 22:34:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,12 +63,19 @@ def app_ui():
                     
                     # Option to download refined tags and comments
                     download_refined_tags_and_comments(refined_tags, comments)
+                                    
+                    
             else:
                 st.write("No videos found.")
 
 def download_refined_tags_and_comments(refined_tags, comments):
     combined_text = f"Refined Tags:\n{refined_tags}\n\nGenerated Comments:\n{comments}"
     st.download_button("Download Refined Tags and Comments", combined_text, "text/plain", "refined_tags_comments.txt")
+
+def download_as_csv(dataframe):
+    # This function converts a DataFrame to CSV and allows the user to download it
+    csv = dataframe.to_csv(index=False)
+    st.download_button(label="Download data as CSV", data=csv, file_name='youtube_data.csv', mime='text/csv')
 
 if __name__ == "__main__":
     app_ui()
